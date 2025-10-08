bc1d683126e6631d01790f507dd22a7230513b61
DEvmIb <DEvmIb@users.noreply.github.com>
2025-10-08 08:01:49 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+def test(p):
+    try:
+        t = __import__(p)
+        print(f'{p} ok')
+    except:
+        print(f'{p} not ok')
+
+
+list = [ 'bs4','cheroot','signal','traceback','urllib', 'curl_cffi','bottle','xmltodict','json','requests','datetime','time','uuid','re','os','threading','hmac','hashlib','base64','gzip','lzma' ]
+
+for p in list:
+    test(p)
+
+try:
+    import os
+    if not os.path.isdir('/usr/share/zoneinfo') or not os.path.isfile('/usr/share/zoneinfo/zone1970.tab') or not os.path.isfile('/usr/share/zoneinfo/tzdata.zi'):
+        print('print missing tzdata')
+    else:
+        print('tzdata ok')
+except:
+    print('missing os module for system checks')
